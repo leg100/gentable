61886b42ab0845e5f93f639436b23b8fda583363
Louis Garman <louisgarman@gmail.com>
2025-01-30 08:25:28 +0000

    wip

---
+++ books.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/lipgloss/table"
+)
+
+var books = []struct {
+	title  string
+	author string
+}{
+	{
+		title:  "The German Ideology",
+		author: "Marx & Engels",
+	},
+	{
+		title:  "Death in Venice and Other Stories",
+		author: "Thomas Mann",
+	},
+	{
+		title:  "Money",
+		author: "Martin Amis",
+	},
+	{
+		title:  "London Fields",
+		author: "Martin Amis",
+	},
+	{
+		title:  "Nana",
+		author: "Emile Zola",
+	},
+	{
+		title:  "To Have and Have Not",
+		author: "Ernest Hemingway",
+	},
+	{
+		title:  "The Sun Also Rises",
+		author: "Ernest Hemingway",
+	},
+	{
+		title:  "A Farewell to Arms",
+		author: "Ernest Hemingway",
+	},
+	{
+		title:  "James Joyce",
+		author: "Ulysses",
+	},
+	{
+		title:  "Trans-Europe Express",
+		author: "Owen Hatherley",
+	},
+}
+
+func main() {
+	t := table.New().Headers("title", "author").
+		Width(30).
+		Height(8).
+		DisableOverflowRow()
+	for _, book := range books {
+		t.Row(book.title, book.author)
+	}
+	t.Offset(2)
+	fmt.Println(t.String())
+}
